dd1b882946b2041dd3906befaa9fb92176990948
Seiwert <adamseiwert@gmail.com>
2016-09-07 20:18:48 -0500

    Slowly remembering how to web dev

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+var controller = require('./endpoints/controller.js');
+app.get('/', controller.getIndex);
+
+
 app.listen(8080, () => {
     console.log('Listening on port 8080.');
 });
---
+++ endpoints/controller.js
@@ -0,0 +1,12 @@
+"use strict"
+
+class Controller {
+    getIndex(req, res) {
+
+        res.render('index', {
+
+        });
+    }
+}
+
+module.exports = exports = new Controller();
